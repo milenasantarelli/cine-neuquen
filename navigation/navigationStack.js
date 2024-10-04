2dcb0ef6cf2b6674842607d4cd0ef3fb20644713
AbrilFuentess <dfabril269@gmail.com>
2024-10-04 19:02:26 -0300

    el menu de fechas wey

--- navigation/navigationStack.js
+++ navigation/navigationStack.js
@@ -22,14 +22,13 @@ const Stack = createNativeStackNavigator();
 const NavigationStack = () => {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator initialRouteName="RegisterLogin"> */}
-      <Stack.Navigator initialRouteName="Principal">
+      <Stack.Navigator initialRouteName="RegisterLogin">
+      {/* <Stack.Navigator initialRouteName="Principal"> */}
       
         <Stack.Screen name="Registro" component={RegistroScreen} />
 
         <Stack.Screen name="RegisterLogin" component={RegisterScreen} options={{ title: 'Registro de Login' }} />
       
-
         <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Iniciar Sesión' }} />
       
         <Stack.Screen name="Tabs" component={TabNavigation} options={{ headerShown: false }} />
