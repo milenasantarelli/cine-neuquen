22ddd183fe26e3e5607b75c67d440e5e726bd415
erickcl23 <clerick2312@gmail.com>
2024-10-23 00:14:49 -0300

    Haciendo merge entre milena y erick

--- screens/registroScreen.js
+++ screens/registroScreen.js
@@ -1,23 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import appFirebase from '../credenciales'; 
+import LoadingScreen from '../assets/components/loadingScreen';  
+import * as SplashScreen from 'expo-splash-screen'; 
 
 const auth = getAuth(appFirebase);
 
 export default function RegisterScreen(props) {
+  const [isLoading, setIsLoading] = useState(true);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
 
+  useEffect(() => {  
+    const prepare = async () => {  
+
+    await new Promise(resolve => setTimeout(resolve,2000));  
+    setIsLoading(false);  
+    await SplashScreen.hideAsync();
+
+    };  
+   
+    prepare();  
+    }, []);  
+   
+    if (isLoading) {  
+    return <LoadingScreen />;  
+    }  
   const registro = async () => {
+  
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+      
+      await createUserWithEmailAndPassword(auth, email, password, name);
       Alert.alert('Registro exitoso', 'Usuario creado correctamente');
       props.navigation.navigate('Principal');  
     } catch (error) {
       console.log(error);
       Alert.alert('Error', error.message);
-    }
+    } 
   };
 
   return (
@@ -32,7 +53,7 @@ export default function RegisterScreen(props) {
         <TextInput
           style={styles.input}
           placeholder="Nombre de Usuario"
-          onChangeText={(e) => setNombre(e)}
+          onChangeText={(text) => setNombre(text)}
         />
         <TextInput
           style={styles.input}
